45bf4ed53e4a338dcd890d5ddee6cef6688cce30
lastweakness <lastweakness@tuta.io>
2020-04-13 18:13:09 +0530

    More documentation improvements
    For no real reason...

--- README.md
+++ README.md
@@ -1,32 +1,40 @@
 # pkgup
+
 A python script for easily updating PKGBUILDs. Made specifically for my packages. Can work for others if PKGBUILD is written accordingly. But it's only suitable for smaller packages with good build systems.
 
-### What does it do?
- - Increases the pkgver and pkgrel to desired values.
- - Downloads the source as '.tar.gz'
- - Verifies basic integrity (`gzip -t`) and re-downloads if necessary.
- - Generates SHA256 checksum of source file.
- - Updates PKGBUILD accordingly.
- - Updates .SRCINFO accordingly. (`makepkg --printsrcinfo`)
+## What pkgup does
 
-### Dependencies
- - `python-tqdm`
- - `python-requests`
- - `gzip`
- - `makepkg` (part of `pacman`, used only for generating `.SRCINFO`)
+- Increases the pkgver and pkgrel to desired values.
+- Downloads the source as '.tar.gz'
+- Verifies basic integrity (`gzip -t`) and re-downloads if necessary.
+- Generates SHA256 checksum of source file.
+- Updates PKGBUILD accordingly.
+- Updates .SRCINFO accordingly. (`makepkg --printsrcinfo`)
 
-### What is needed in the PKGBUILD for upgradablity?
- - It only works with '.tar.gz' source files.
- - Your `source` link should use `$pkgver` for downloading the correct version. For example, it can be like:  
- https://gitlab.com/valos/Komikku/-/archive/v$pkgver/Komikku-v$pkgver.tar.gz
+## Dependencies
 
-#### Preferred format
+- `python-tqdm`
+- `python-requests`
+- `gzip`
+- `makepkg` (part of `pacman`, used only for generating `.SRCINFO`)
+
+## What's needed in the PKGBUILD for upgradablity
+
+- It only works with '.tar.gz' source files.
+- Your `source` link should use `$pkgver` for downloading the correct version. For example, it can be like: <https://gitlab.com/valos/Komikku/-/archive/v$pkgver/Komikku-v$pkgver.tar.gz>
+
+### Preferred format
+
 The preferred format for `source` is as follows. For projects hosted on GitLab:
+
 ```
 source=('https://www.gitlab.com/$_author/$_gitname/-/archive/v$pkgver/$_gitname-v$pkgver.tar.gz')
 ```
+
 For projects hosted on GitHub:
+
 ```
 source=('https://www.github.com/$_author/$_gitname/releases/archive/v$pkgver.tar.gz')
 ```
+
 `_author` and `$_gitname` are not absolutely necessary, but recommended.
--- pkgup.py
+++ pkgup.py
@@ -37,15 +37,18 @@ class PkgUp():
         with open(src_file_path, "rb") as src_file:
             # The file may be large, read it as blocks of 65536 bytes (64 KBs).
             for block in iter(lambda: src_file.read(65536), b""):
+                # Update the sha256 block-by-block.
                 sha256.update(block)
         return sha256.hexdigest()
 
     def source_process(self):
         """Process the source link."""
+        # Get the package name.
         pkgname = re.search("pkgname=.*",
                             self.pkgbuild_content,
                             flags=re.IGNORECASE
                             ).group(0).replace("pkgname=", "")
+        # Get the git repository name.
         self.gitname = re.search("_gitname=.*", self.pkgbuild_content,
                                  flags=re.IGNORECASE).group(0)
         if self.gitname is None:
@@ -53,6 +56,7 @@ class PkgUp():
             self.gitname = pkgname
         else:
             self.gitname = self.gitname.replace("_gitname=", "")
+        # Get the author name.
         author = re.search("_author=.*", self.pkgbuild_content,
                            flags=re.IGNORECASE).group(0)
         if author is None:
@@ -60,26 +64,32 @@ class PkgUp():
                   " $_author is used in source.")
         else:
             author = author.replace("_author=", "")
+        # Get the source link.
         source = re.search("source=.*", self.pkgbuild_content,
                            flags=re.IGNORECASE).group(0).replace("source=", "")
+        # Clean the source link.
         tar_file_name = f"{self.gitname}-{ARGS.pkgver}.tar.gz"
-        link_clean_list = {"$_author": author,
-                           "$_gitname": self.gitname,
-                           "$pkgver": ARGS.pkgver,
-                           "$pkgname": pkgname,
-                           '("': '',
-                           '")': '',
-                           tar_file_name + "::": ''}
+        link_clean_list = {
+            "$_author": author,
+            "$_gitname": self.gitname,
+            "$pkgver": ARGS.pkgver,
+            "$pkgname": pkgname,
+            '("': '',
+            '")': '',
+            tar_file_name + "::": ''
+        }
         for old_val, new_val in link_clean_list.items():
             source = source.replace(old_val, new_val)
         return source
 
     def integrity_check(self, file_name):
         """Check the integrity of the source file and re-download if needed."""
+        # Use gzip to check basic integrity of the downloaded file.
         check = subprocess.Popen(["gzip", "-t", file_name],
                                  stdout=subprocess.PIPE,
                                  stderr=subprocess.PIPE)
-        check.wait()
+        check.wait()  # Wait for process completion.
+        # Check the return code of the program to know if the file is broken.
         if check.returncode == 0:
             print("File seems okay. Proceed.")
         else:
@@ -89,15 +99,21 @@ class PkgUp():
 
     def pkgbuild_update(self, pkgver, new_hash):
         """Update the PKGBUILD according to the changes."""
+        # Get the old sha256sums.
         sha256sums = re.search('sha256sums=.*', self.pkgbuild_content,
                                flags=re.IGNORECASE).group(0)
+        # Get the old version number.
         old_ver = re.search('pkgver=.*', self.pkgbuild_content,
                             flags=re.IGNORECASE).group(0)
+        # Get the old release number.
         old_rel = re.search('pkgrel=.*', self.pkgbuild_content,
                             flags=re.IGNORECASE).group(0)
-        update_list = {sha256sums: f"sha256sums=('{new_hash}')",
-                       old_ver: f"pkgver={pkgver}",
-                       old_rel: f"pkgrel={str(ARGS.pkgrel or 1)}"}
+        # Update the PKGBUILD with new values.
+        update_list = {
+            sha256sums: f"sha256sums=('{new_hash}')",
+            old_ver: f"pkgver={pkgver}",
+            old_rel: f"pkgrel={str(ARGS.pkgrel or 1)}"
+        }
         pkgbuild_new = self.pkgbuild_content
         for old_val, new_val in update_list.items():
             pkgbuild_new = pkgbuild_new.replace(old_val, new_val)
@@ -108,10 +124,12 @@ class PkgUp():
     @staticmethod
     def srcinfo_update():
         """Update the .SRCINFO file."""
+        # Use makepkg to generate the .SRCINFO output.
         srcinfo_gen = subprocess.Popen(["makepkg", "--printsrcinfo"],
                                        stdout=subprocess.PIPE,
                                        stderr=subprocess.STDOUT)
-        srcinfo_gen.wait()
+        srcinfo_gen.wait()  # Wait for process completion.
+        # Save output to .SRCINFO file.
         srcinfo = srcinfo_gen.stdout.read().decode()
         with open('.SRCINFO', 'w') as srcinfo_file:
             srcinfo_file.write(srcinfo)
@@ -119,12 +137,17 @@ class PkgUp():
 
     def download_src(self, repeat=False):
         """Download the source file and run integrity_check."""
-        source = self.source_process()
+        source = self.source_process()  # Get source link.
+        # Get downloaded source file name.
         src_file_name = f"{self.gitname}-v{ARGS.pkgver}.tar.gz"
+        # Check if the file is being re-downloaded.
         if not repeat:
+            # Print this only once.
             print("Download source processed as: " + source)
             print("File name will be: " + src_file_name)
+        # Check if file already exists.
         if not os.path.exists(src_file_name):
+            # Use requests to get the file and tqdm to update progress.
             request = requests.get(source, stream=True)
             file_size = int(request.headers.get('content-length', 0))
             prog = tqdm(total=file_size, unit='iB', unit_scale=True)
@@ -134,16 +157,19 @@ class PkgUp():
                     src_file.write(data)
                     src_file.flush()
             prog.close()
-        self.integrity_check(src_file_name)
-        return src_file_name
+        self.integrity_check(src_file_name)  # Check file integrity.
+        return src_file_name  # Return the file name.
 
     def main(self):
         """Read PKGBUILD and run other functions in order."""
         with open('PKGBUILD', 'r') as pkgbuild:
             self.pkgbuild_content = pkgbuild.read()
+        # Donwload the source file and get its file name.
         src_file_name = self.download_src()
+        # Get the SHA256 sum of the source file.
         src_hash = self.file_hasher(src_file_name)
         print("SHA256 Sum found to be: " + src_hash)
+        # Update the PKGBUILD with the new values.
         self.pkgbuild_update(ARGS.pkgver, src_hash)
         self.srcinfo_update()
 
